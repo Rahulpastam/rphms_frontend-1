20dfd81cd74c485a9b55bbbcb231b0ea113b09d6
Rahulpastam <pastam.rahul@gmail.com>
2024-10-03 17:43:58 +0530

    "Update link in Register page from '/signin' to '/login'"

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -133,7 +133,7 @@ const Register = () => {
           >
             <p style={{ marginBottom: 0 }}>Already Registered?</p>
             <Link
-              to={"/signin"}
+              to={"/login"}
               style={{ textDecoration: "none", color: "#271776ca" }}
             >
               Login Now
